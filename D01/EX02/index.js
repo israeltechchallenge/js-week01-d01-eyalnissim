71f23bccbbf6392118052a372ffbfaedee966ac5
Eyal Nissim <eyalnissim11@gmail.com>
2022-06-20 08:51:08 +0300

    week01 day01 eyalnissim

--- D01/EX02/index.js
+++ D01/EX02/index.js
@@ -1,5 +1,18 @@
-function isBiggerThan() {
+function isBiggerThan(i) {
   //your code
+  
+  if (i > 4) {
+    console.log(`The number ${i} is bigger than 4`);
+}
+
+else if (i < 4){
+    console.log(`The number ${i} is less than 4`);
+}
+
+else  {
+    console.log(`The number ${i} is equal to 4`);
+}
+
 }
 
 // Do not remove or change this line, or the tests won't work
--- D01/EX07/index.js
+++ D01/EX07/index.js
@@ -1,7 +1,32 @@
-function getRandomInt(max) {
+function getRandomInt() {
   //your code
+    let numA = Math.random()*100;
+    let num1 = parseInt(numA);
+    return num1;
+    // let numB = math.random()*100;
+    // let num2= parseInt(numB);
 }
-
-function moduloEx7() {
-  //your code
-}
+ let num1 = getRandomInt();
+ let num2 = getRandomInt();
+  function moduloEx7(num1,num2) {
+    //your code
+    let sentence = ""
+    
+    if (num1 > num2 && num1%num2 !==0 ) {
+        sentence=`The number ${num1} is bigger than ${num2}. But the modulo of ${num1} % ${num2} is ${num1%num2}`;
+         
+    }else if (num1>num2){
+      sentence = `The number ${num1} is bigger than ${num2}`
+      
+    }else if (num1 === num2){
+      sentence=`The number ${num1} is equal to ${num2}`;
+    } else  {
+      sentence=`The number ${num1} is less than ${num2}`;
+    }
+    return sentence;
+  
+  }
+  
+  document.getElementById("output").innerText= moduloEx7(num1,num2);
+  
+  
